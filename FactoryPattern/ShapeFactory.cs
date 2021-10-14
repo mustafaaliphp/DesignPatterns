fc31e7cc603328a553ab83b2435024cc347d191b
musta <musta@DESKTOP-U98RRHN>
2021-10-13 20:19:40 -0500

    Add project files.

---
+++ FactoryPattern/ShapeFactory.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FactoryPattern
+{
+    class ShapeFactory
+    {
+        //use getShape method to get object of type shape 
+        public Shape getShape(String shapeType)
+        {
+            if (shapeType == null)
+            {
+                return null;
+            }
+            if (shapeType.Equals("CIRCLE"))
+            {
+                return new Circle();
+
+            }
+            else if (shapeType.Equals("RECTANGLE"))
+            {
+                return new Rectangle();
+
+            }
+            else if (shapeType.Equals("SQUARE"))
+            {
+                return new Square();
+            }
+
+            return null;
+        }
+    }
+}
